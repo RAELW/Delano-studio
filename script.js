755ae0b602fea4a6a7d2824469109f72a9bb784e
RAELW <rael.wandera@student.moringaschool.com>
2022-03-15 00:01:17 +0300

    patch:modified html,js and css files

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.design').click((e)=>{
+    e.currentTarget.nextElementSibling.classList.remove('d-none')
+    e.currentTarget.nextElementSibling.classList.add('d-block')
+    $('.design-description').css({'text-align':'center'})
+})
+$('.folio').mouseenter((e)=>{
+   console.log($(this))
+   $(e.currentTarget).find('.title').addClass('d-block')
+   $(e.currentTarget).find('.title').removeClass('d-none')
+})
+$('.folio').mouseleave((e)=>{
+   $(e.currentTarget).find('.title').addClass('d-none')
+   $(e.currentTarget).find('.title').removeClass('d-block')
+})
